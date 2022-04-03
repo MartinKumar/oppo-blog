2372b5d1c25d3f28bb0a012139d8913b130afdb6
Martin h <martinhen737@gmail.com>
2022-04-03 16:22:13 +0530

    create new branch and update code in it

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ echo "Thanks<br>";
 
 date_default_timezone_set("Asia/Calcutta"); 
 echo date('d/m/Y h:i:s A', time());
-echo "<br>Learn git all chapters. test";
+echo "<br>create new branch in github";
 
 ?>
